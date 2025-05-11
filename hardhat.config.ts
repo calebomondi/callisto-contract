c08474fdebc8ad3a13ee53974eebeb8708b21673
calebomondi <caleb.omondi@students.jkuat.ac.ke>
2025-05-11 18:10:26 +0300

    Add deployment script for Main, LendManager, and LockAsset contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require('dotenv').config();
+
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.28",
@@ -12,7 +14,38 @@ const config: HardhatUserConfig = {
       viaIR: true,
     },
   },
-  
+  networks: {
+    // for mainnet
+    'base-mainnet': {
+      url: 'https://mainnet.base.org',
+      accounts: [process.env.WALLET_KEY as string],
+      gasPrice: 1000000000,
+    },
+    // for testnet
+    'base-sepolia': {
+      url: 'https://sepolia.base.org',
+      accounts: [process.env.WALLET_KEY as string],
+      gasPrice: 1000000000,
+    }
+  },
+  etherscan: {
+    apiKey: {
+    "base-sepolia": process.env.BLOCKSCOUT_KEY as string
+    },
+    customChains: [
+      {
+        network: "base-sepolia",
+        chainId: 84532,
+        urls: {
+        apiURL: "https://base-sepolia.blockscout.com/api",
+        browserURL: "https://base-sepolia.blockscout.com"
+        }
+      }
+    ]
+  },
+  sourcify: {
+    enabled: false
+  }
 };
 
 export default config;
---
+++ scripts/deploy.ts
@@ -0,0 +1,23 @@
+import { ethers } from 'hardhat';
+ 
+async function main() {
+    //deploy main contract
+    const main = await ethers.deployContract('Main');
+    await main.waitForDeployment();
+    console.log('Main Contract Deployed at ' + main.target);
+
+    //deploy LendManager then contract
+    const lendmanager = await ethers.deployContract('LendManager');
+    await lendmanager.waitForDeployment();
+    console.log('LendManager Contract Deployed at ' + lendmanager.target);
+
+    //deploy LockAsset contract
+    const lockasset = await ethers.deployContract('LockAsset', [lendmanager.target]);
+    await lockasset.waitForDeployment();
+    console.log('LockAsset Contract Deployed at ' + lockasset.target);
+}
+ 
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
